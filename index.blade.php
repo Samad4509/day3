7a101e75f5593cd998a882f216f25c524da255bf
Samad4509 <99247399+Samad4509@users.noreply.github.com>
2023-04-11 10:54:45 +0600

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+       <table border="1" width="1200" align="center">
+           <tr>
+               <td height="200" width="200">
+                   <img src="img/1.jpg" alt="" height="200" width="200"/>
+               <td></td>
+           </tr>
+           <tr>
+               <td height="50" colspan="2"></td>
+           </tr>
+           <tr>
+               <td height="600"></td>
+               <td valign="top">
+                   <h1>This is a heading</h1>
+                   <p>
+                       <ins>Lorem ipsum dolor sit amet,</ins> <em> consectetur adipisicing elit.</em> <mark>Corporis culpa, cumque deleniti deserunt,</mark> dolorem eligendi fuga, fugiat incidunt maxime odio officiis porro possimus quibusdam. Facilis impedit ipsum molestias neque voluptate.
+                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis culpa, cumque deleniti deserunt, dolorem eligendi fuga, fugiat incidunt maxime odio officiis porro possimus quibusdam. Facilis impedit ipsum molestias neque voluptate
+                   </p>
+
+                   <p>
+                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis culpa, cumque deleniti deserunt, dolorem eligendi fuga, fugiat incidunt maxime odio officiis porro possimus quibusdam. Facilis impedit ipsum molestias neque voluptate.
+                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis culpa, cumque deleniti deserunt, dolorem eligendi fuga, fugiat incidunt maxime odio officiis porro possimus quibusdam. Facilis impedit ipsum molestias neque voluptate
+                   </p>
+                   <p>
+                       (a+b)<sup>2</sup>=a2+2ab+b<sup>2</sup>
+                   </p>
+                   <p>
+                       H <sub>2</sub>O
+                   </p>
+                   <p>
+                       <span>&pi;</span> r<sup>2</sup>
+                   </p>
+                   <p>
+                       <span> &lt;</span>h1<span> &gt;</span> this is the heding <span> &lt;</span>/h1<span> &gt;</span>
+                   </p>
+               </td>
+           </tr>
+           <tr>
+               <td height="250" colspan="2">
+                   <table border="1" width="1200">
+                       <tr>
+                           <td height="250"></td>
+                           <td></td>
+                           <td></td>
+                       </tr>
+                   </table>
+               </td>
+           </tr>
+           <tr>
+               <td height="50" colspan="2"></td>
+           </tr>
+       </table>
+<body>
+
+</body>
+</html>
